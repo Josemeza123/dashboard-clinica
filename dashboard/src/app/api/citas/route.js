fbee866dcb1a7ae35079c0e89e18d8b40fe7dfac
Jose Meza <enriquesais123@gmail.com>
2024-05-13 17:43:10 -0500

    avance

--- dashboard/src/app/api/citas/route.js
+++ dashboard/src/app/api/citas/route.js
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server";
+import { conn } from "../../libs/mysql";
+
 export async function GET() {
   try {
     const result = await conn.query("SELECT * FROM citas");
@@ -9,15 +12,30 @@ export async function GET() {
 
 export async function POST(request) {
   try {
-    const { name, descripcion, price } = await request.json();
+    const {
+      id_terapeuta,
+      id_paciente,
+      fecha_atencion,
+      tratamiento,
+      observacion,
+    } = await request.json();
 
     const result = await conn.query("INSERT INTO citas SET ?", {
-      name,
-      descripcion,
-      price,
+      id_terapeuta,
+      id_paciente,
+      fecha_atencion,
+      tratamiento,
+      observacion,
     });
     console.log(result);
-    return NextResponse.json({ name, descripcion, price, id: result.insertId });
+    return NextResponse.json({
+      id_terapeuta,
+      id_paciente,
+      fecha_atencion,
+      tratamiento,
+      observacion,
+      id: result.insertId,
+    });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: error.message }, { status: 500 });
--- dashboard/src/app/api/products/[id]/route.js
+++ dashboard/src/app/api/products/[id]/route.js
@@ -1,13 +1,59 @@
+import { conn } from "@/src/app/libs/mysql";
 import { NextResponse } from "next/server";
 
-export function GET() {
-  return NextResponse.json("Hello World un dato!");
+// pidiendo un producto por su id
+export async function GET(request, { params }) {
+  try {
+    const result = await conn.query("SELECT * FROM product WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.length === 0)
+      return NextResponse.json(
+        {
+          message: "Product not found",
+        },
+        { status: 404 }
+      );
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
 
-export function DELETE() {
-  return NextResponse.json("Hello World delete!");
+// actualizando un producto por su id
+export async function PUT(request, { params }) {
+  try {
+    const data = await request.json();
+    const result = await conn.query("UPDATE product SET ? WHERE id = ?", [
+      data,
+      params.id,
+    ]);
+    console.log(result);
+    return NextResponse.json({ ...data });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
 
-export function PUT() {
-  return NextResponse.json("Hello World put!");
+// eliminando un producto por su id
+export async function DELETE(request, { params }) {
+  try {
+    const result = await conn.query("DELETE FROM product WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.affectedRows === 0) {
+      return NextResponse.json(
+        {
+          message: "Product not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return new Response(null, {
+      status: 204,
+    });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
